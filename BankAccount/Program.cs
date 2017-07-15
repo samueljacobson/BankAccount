b6b377fb93b129001cfa6704e0348a6e111f3853
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-15 12:29:58 -0400

    edits

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -10,63 +10,68 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
-            Client johnSmith = new Client();
-            CheckingAccount jsChecking = new CheckingAccount();
-            SavingsAccount jsSavings = new SavingsAccount();
+            int selection;
+            do
+            {
+                Client johnSmith = new Client();
+                CheckingAccount jsChecking = new CheckingAccount();
+                SavingsAccount jsSavings = new SavingsAccount();
 
-            Console.WriteLine("Hello and welcome to Lehman Brothers. \nHere you can view your account information.");
-            Console.WriteLine("It is 2008 and we promise that subprime lending is always a wise investment.");
-            Console.WriteLine("We also guarantee to never file for bankruptcy.");
+                Console.WriteLine("\nHello and welcome to Lehman Brothers. \nHere you can view your account information.");
+                Console.WriteLine("It is 2008 and we promise that subprime lending is always a wise investment.");
+                Console.WriteLine("We also guarantee to never file for bankruptcy.");
 
-            Console.WriteLine("\nPlease select from the following options: \n1. View Client Informationn\n2. View Account Balance\n3. Deposit Funds\n4. Withdraw Funds\n5. Exit");
-            int selection = int.Parse(Console.ReadLine());
-            char account;
-            if (selection == 1)
-            {
-                Console.WriteLine(johnSmith.GetClientInfo());
+                Console.WriteLine("\nPlease select from the following options: \n1. View Client Informationn\n2. View Account Balance\n3. Deposit Funds\n4. Withdraw Funds\n5. Exit");
+                selection = int.Parse(Console.ReadLine());
+                char account;
+                if (selection == 1)
+                {
+                    Console.WriteLine(johnSmith.GetClientInfo());
+                }
+                if (selection == 2)
+                {
+                    Console.WriteLine("Which account would you like to view the balance of?");
+                    Console.WriteLine("Please select:\na. Checking Account\nb. Savings Account");
+                    account = char.Parse(Console.ReadLine().ToLower());
+                    if (account == 'a')
+                    {
+                        jsChecking.GetBalance();
+                    }
+                    if (account == 'b')
+                    {
+                        jsSavings.GetBalance();
+                    }
+                }
+                if (selection == 3)
+                {
+                    Console.WriteLine("To which account would you like to make a deposit?");
+                    Console.WriteLine("Please select:\na. Checking Account\nb. Savings Account");
+                    account = char.Parse(Console.ReadLine().ToLower());
+                    if (account == 'a')
+                    {
+                        jsChecking.Deposit();
+                    }
+                    if (account == 'b')
+                    {
+                        jsSavings.Deposit();
+                    }
+                }
+                if (selection == 4)
+                {
+                    Console.WriteLine("From which account would you like to make a withdrawal?");
+                    Console.WriteLine("Please select:\na. Checking Account\nb. Savings Account");
+                    account = char.Parse(Console.ReadLine().ToLower());
+                    if (account == 'a')
+                    {
+                        jsChecking.Withdraw();
+                    }
+                    if (account == 'b')
+                    {
+                        jsSavings.Withdraw();
+                    }
+                }
             }
-            if (selection == 2)
-            {
-                Console.WriteLine("Which account would you like to view the balance of?");
-                Console.WriteLine("Please select:\na. Checking Account\nb. Savings Account");
-                account = char.Parse(Console.ReadLine().ToLower());
-                if (account == 'a')
-                {
-                    jsChecking.GetBalance();
-                }
-                if (account == 'b')
-                {
-                    jsSavings.GetBalance();
-                }
-            }
-            if (selection == 3)
-            {
-                Console.WriteLine("To which account would you like to make a deposit?");
-                Console.WriteLine("Please select:\na. Checking Account\nb. Savings Account");
-                account = char.Parse(Console.ReadLine().ToLower());
-                if (account == 'a')
-                {
-                    jsChecking.Deposit();
-                }
-                if (account == 'b')
-                {
-                    jsSavings.Deposit();
-                }
-            }
-            if (selection == 4)
-            {
-                Console.WriteLine("From which account would you like to make a withdrawal?");
-                Console.WriteLine("Please select:\na. Checking Account\nb. Savings Account");
-                account = char.Parse(Console.ReadLine().ToLower());
-                if (account == 'a')
-                {
-                    jsChecking.Withdraw();
-                }
-                if (account == 'b')
-                {
-                    jsSavings.Withdraw();
-                }
-            }
+            while (selection != 5);
         }
     }
 }
--- BankAccount/SavingsAccount.cs
+++ BankAccount/SavingsAccount.cs
@@ -9,11 +9,14 @@ namespace BankAccount
     class SavingsAccount : Account
     {
         protected int accountNumber = 189007;
-        protected double balance = 24517.00d;
+        protected double balance = 25000.00d;
         protected string accountType = "Savings";
 
         private double deposit;
         private double withdrawal;
+        private double minBalance = 1000d;
+
+        public double MinBalance { get; }
 
         public SavingsAccount()
         {
@@ -34,9 +37,24 @@ namespace BankAccount
 
         public override void Withdraw()
         {
-            Console.WriteLine("Enter the amount of your withdrawal:");
-            withdrawal = double.Parse(Console.ReadLine());
-            Console.WriteLine("Thank you. Your current checking account balance is: $" + (balance - withdrawal));
+            while (true)
+            {
+                Console.WriteLine("Enter the amount of your withdrawal:");
+                withdrawal = double.Parse(Console.ReadLine());
+
+                if ((balance - withdrawal) < minBalance)
+                {
+                    Console.WriteLine("Sorry, you cannot withdraw below the minimum balance of $" + minBalance);
+                    Console.WriteLine("The maximum withdrawal is $" + (balance - minBalance) + ". Please try again.");
+                }
+
+                if ((balance - withdrawal) >= minBalance)
+                {
+                    Console.WriteLine("Thank you. Your current checking account balance is: $" + (balance - withdrawal));
+                    break;
+                }
+            }   
+            
         }
     }
 }
